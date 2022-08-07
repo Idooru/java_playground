b0174aef9ec6448e4d91e410cd3231c0357b919b
Idooru <shere1765@gmail.com>
2022-08-07 14:07:19 +0900

    learn boolean

---
+++ src/egoing/basics/compare/EqualDemo.java
@@ -0,0 +1,52 @@
+package egoing.basics.compare;
+
+public class EqualDemo {
+    public static void main(String[] args) {
+        System.out.println(1 == 2);
+        System.out.println(1 == 1);
+        System.out.println("one" == "two");
+        System.out.println("one" == "one");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/egoing/basics/compare/GreaterThenOrEqualDemo.java
@@ -0,0 +1,9 @@
+package egoing.basics.compare;
+
+public class GreaterThenOrEqualDemo {
+    public static void main(String[] args) {
+        System.out.println(10 >= 20);
+        System.out.println(10 >= 2);
+        System.out.println(10 >= 10);
+    }
+}
