f8eacd70c7f3076e09c864f6915bb79033b98693
Idooru <shere1765@gmail.com>
2022-08-10 14:20:39 +0900

    practice getter and setter

---
+++ src/myPractice/GetterAndSetterDemo.java
@@ -0,0 +1,14 @@
+package myPractice;
+
+import myPractice.Models.Man;
+
+
+public class GetterAndSetterDemo {
+    public static void main(String[] args) {
+        Man man1 = new Man();
+        Man man2 = new Man("Idooru", 25);
+
+        System.out.println(man1);
+        System.out.println(man2);
+    }
+}
