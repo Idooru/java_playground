20888b63433be7110cbd26ad4154412f4b1e53b1
Idooru <shere1765@gmail.com>
2022-08-16 12:07:54 +0900

    practice overriding

--- src/egoing/advanced/collectionsframework/CollectionsDemo.java
+++ src/egoing/advanced/collectionsframework/CollectionsDemo.java
@@ -1,9 +1,9 @@
-package egoing.advanced.collectionsframework;
-
-import java.util.List;
-
-public class CollectionsDemo {
-    public static void main(String[] args) {
-        List<Computer>
-    }
-}
+//package egoing.advanced.collectionsframework;
+//
+//import java.util.List;
+//
+//public class CollectionsDemo {
+//    public static void main(String[] args) {
+//        List<Computer>
+//    }
+//}
