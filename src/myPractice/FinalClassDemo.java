4842f1db845a24d9b2be07048d58c9bbade7f52d
Idooru <shere1765@gmail.com>
2022-08-16 23:02:10 +0900

    Practice final keyword

---
+++ src/myPractice/FinalClassDemo.java
@@ -0,0 +1,19 @@
+package myPractice;
+
+final class Fortune {
+    protected String fortune = "귀중한 재산";
+}
+
+/*
+ * 상속받으려는 상위 클래스가 final로 정의 되어 있어 상속이 불가능해진다.
+ */
+//class Receiver extends Fortune {
+//    void recive() {
+//        System.out.println(super.fortune);
+//    }
+//}
+
+public class FinalClassDemo {
+    public static void main(String[] args) {
+    }
+}
